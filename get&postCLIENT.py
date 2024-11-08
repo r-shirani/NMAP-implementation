7c783f956807fe0af7c2289a3bc117f30403387b
r-shirani <127476888+r-shirani@users.noreply.github.com>
2024-11-08 17:20:05 +0330

    phase1-part4-get&postCLIENT-was done

--- get&postCLIENT.py
+++ get&postCLIENT.py
@@ -1,1 +1,22 @@
+import socket
+
+def main():
+    host = 'localhost'
+    port = 8080
+    while True:
+        input=input("Enter 'GET user_id' or 'POST user_name user_age': ")
+        #creating a socket and connect it to the host
+        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client_socket.connect((host, port))
+        #send the request to the server by converting it from string to binary
+        client_socket.sendall(input.encode())
+        #recieve the response
+        response = client_socket.recv(1024).decode()
+        #show the response
+        print(f"Response from the server: {response}")
+        #closing the socket
+        client_socket.close()
+
+if __name__ == '__main__':
+    main()
 
